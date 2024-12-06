2ced0df6ab42481ac7872f6fac791a672a2be81b
Karthik0-0 <pavankarthik107@gmail.com>
2024-12-06 13:44:36 +0000

    Modified Events section

--- my-app/src/app/UI/Sponcers/page.js
+++ my-app/src/app/UI/Sponcers/page.js
@@ -1,6 +1,7 @@
 import "./sponcers.css"
 import sponcer from "../../Assets/Aavanflix.png"
 import Image from "next/image";
+import Link from "next/link";
 
 
 const Sponcers = () => {
@@ -10,22 +11,24 @@ const Sponcers = () => {
                 <div className="sponcer-container-heading">
                     <h1>Our Sponcers</h1>
                 </div>
-                <div className="sponcers-container-in-content">
-                    <div className="sponcers-container-in-one">
-                        <div className="sponcer-container-in-one-image">
-                            <Image src={sponcer} alt="Sponcer"  width={400} height={150} />
-                        </div>
-                        {/* <div className="sponcer-container-in-one-description">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque facere id quia, vel error necessitatibus veniam saepe quis, cumque molestiae dignissimos dolorem quas placeat quos cum deserunt adipisci voluptates sit accusamus eos deleniti eum corrupti. Excepturi voluptates corrupti dolorum animi impedit qui quas, voluptatibus illo laborum reiciendis. Corrupti, numquam sint?
-                        </div> */}
+                <div className="sponcers-container-in-one">
+                    <div className="sponcer-container-in-one-image">
+                        <Link href="https://www.instagram.com/avanflix/?hl=en" legacyBehavior>
+                            <a target="_blank" rel="noopener noreferrer"> <Image src={sponcer} alt="Sponcer"  width={300} height={100} /> </a>
+                        </Link>
                     </div>
-                    <div className="sponcers-container-in-one">
-                        <div className="sponcer-container-in-one-image">
-                            <Image src={sponcer} alt="Sponcer"  width={400} height={150} />
-                        </div>
-                        {/* <div className="sponcer-container-in-one-description">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque facere id quia, vel error necessitatibus veniam saepe quis, cumque molestiae dignissimos dolorem quas placeat quos cum deserunt adipisci voluptates sit accusamus eos deleniti eum corrupti. Excepturi voluptates corrupti dolorum animi impedit qui quas, voluptatibus illo laborum reiciendis. Corrupti, numquam sint?
-                        </div> */}
+                    <div className="sponcer-container-in-one-description">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque facere id quia, vel error necessitatibus veniam saepe quis, cumque molestiae dignissimos dolorem quas placeat quos cum deserunt adipisci voluptates sit accusamus eos deleniti eum corrupti. Excepturi voluptates corrupti dolorum animi impedit qui quas, voluptatibus illo laborum reiciendis. Corrupti, numquam sint?
+                    </div>
+                </div>
+                <div className="sponcers-container-in-one two">
+                    <div className="sponcer-container-in-one-image">
+                        <Link href="https://www.instagram.com/avanflix/?hl=en" legacyBehavior>
+                            <a target="_blank" rel="noopener noreferrer"> <Image src={sponcer} alt="Sponcer"  width={300} height={100} /> </a>
+                        </Link>
+                    </div>
+                    <div className="sponcer-container-in-one-description">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque facere id quia, vel error necessitatibus veniam saepe quis, cumque molestiae dignissimos dolorem quas placeat quos cum deserunt adipisci voluptates sit accusamus eos deleniti eum corrupti. Excepturi voluptates corrupti dolorum animi impedit qui quas, voluptatibus illo laborum reiciendis. Corrupti, numquam sint?
                     </div>
                 </div>
             </div>
