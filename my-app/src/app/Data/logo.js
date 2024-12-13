6f1918ae452837402bd1156c2990375d96d16578
Karthik0-0 <pavankarthik107@gmail.com>
2024-12-13 12:51:41 +0000

    Removed Student tribe logo

--- my-app/src/app/Data/logo.js
+++ my-app/src/app/Data/logo.js
@@ -3,10 +3,10 @@ export const logo = [
         logo: "https://i.imghippo.com/files/gFS4183wk.jpeg",
         url:"https://klzeroone.vercel.app/"
     },
-    {
-        logo: "/logo/student_tribe.png",
-        url: "https://www.studenttribe.in/"
-    },
+    // {
+    //     logo: "/logo/student_tribe.png",
+    //     url: "https://www.studenttribe.in/"
+    // },
     {
         logo: "/logo/hand.png",
         url: "https://www.instagram.com/avanflix/?hl=en"
